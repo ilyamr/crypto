0ebb9415fc9c20c9efae663503b856ebede0426a
Martirosyan Mrd <martirosyanmrd@gmail.com>
2021-07-23 19:32:43 +0400

    changes

--- src/containers/LogIn/index.jsx
+++ src/containers/LogIn/index.jsx
@@ -1,54 +1,69 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+// import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-import { noop } from "../../utils";
+// import { noop } from "../../utils";
 
 import styles from "./Login.module.scss";
 
-const Login = ({ emailValue, emailChange, passwordValue, passwordChange }) => {
+const Login = () => {
   const [ourUser, setOurUser] = useState(false);
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  const emailHandler = (e) => {
+    setEmailValue(e.target.value.trim());
+  };
+
+  const passwordHandler = (e) => {
+    setPasswordValue(e.target.value.trim());
+  };
+
+  useEffect(() => {
+    if (emailValue.trim() !== "" && passwordValue.trim() !== "") {
+      setOurUser(!ourUser);
+    }
+  }, [emailValue, passwordValue]);
 
   return (
     <div className={styles.login}>
       <h1 className={styles.login__title}>Login</h1>
       <input
-        type="text"
+        type="email"
         value={emailValue}
         placeholder="Email"
-        onChange={emailChange}
+        onChange={emailHandler}
         className={styles.login__email}
       />
       <input
         type="password"
         value={passwordValue}
         placeholder="Password"
-        onChange={passwordChange}
+        onChange={passwordHandler}
         className={styles.login__password}
       />
-      <Link className={styles.login__log_btn} to="/users">
+      <Link
+        className={ourUser ? styles.login__log_btn : styles.login__disabled_btn}
+        title={ourUser ? "Allow" : "Disabled"}
+        to={ourUser ? "/users" : ""}
+        type="button"
+      >
         Login
       </Link>
-      <Link to="/registration" className={styles.login__reg_btn}>
+      <Link className={styles.login__reg_btn} to="/registration">
         Register
       </Link>
-      <span className={styles.login__frgt_btn}>Forgot Password</span>
+      {/* <span className={styles.login__frgt_btn}>Forgot Password</span> */}
     </div>
   );
 };
 
-Login.propTypes = {
-  emailChange: PropTypes.func,
-  emailValue: PropTypes.string,
-  passwordChange: PropTypes.func,
-  passwordValue: PropTypes.string,
-};
+// Login.propTypes = {
+//  passwordValue: PropTypes.string,
+// };
 
-Login.defaultProps = {
-  emailValue: "",
-  emailChange: noop,
-  passwordValue: "",
-  passwordChange: noop,
-};
+// Login.defaultProps = {
+//  passwordChange: noop,
+// };
 
 export default Login;
